110b46adfd7bc6a4ac3a3c919dd3467c14464b51
DzakySalman <evoyege@gmail.com>
2024-06-13 15:09:10 +0700

    B
    B

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,7 +40,8 @@ dependencies {
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
     implementation("com.loopj.android:android-async-http:1.4.9")
-    implementation("com.google.code.gson:gson:2.8.5")
+    implementation("com.google.code.gson:gson:2.10")
     implementation("androidx.recyclerview:recyclerview:1.3.2")
     implementation("androidx.cardview:cardview:1.0.0")
+    implementation("com.google.android.material:material:1.12.0")
 }
--- app/src/main/java/com/apyogananta/recyclerviewb/MainActivity.java
+++ app/src/main/java/com/apyogananta/recyclerviewb/MainActivity.java
@@ -1,16 +1,22 @@
 package com.apyogananta.recyclerviewb;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 import com.loopj.android.http.AsyncHttpClient;
 import com.loopj.android.http.AsyncHttpResponseHandler;
+
+import java.util.List;
 
 import cz.msebera.android.httpclient.Header;
 
@@ -31,7 +37,7 @@ public class MainActivity extends AppCompatActivity {
         _recyclerView1 = (RecyclerView) findViewById(R.id.recyclerView1);
         _txtMahasiswaCount = findViewById(R.id.txtMahasiswaCount);
 
-        //initAddButton();/
+        initAddButton();
         loadRecyclerView();
     }
 
@@ -42,27 +48,39 @@ public class MainActivity extends AppCompatActivity {
         ahc.get(url, new AsyncHttpResponseHandler() {
             @Override
             public void onSuccess(int statusCode, Header[] headers, byte[] responseBody) {
+                Gson g = new Gson();
+
+                List<MahasiswaModel> mahasiswaModelList = g.fromJson(new String(responseBody), new TypeToken<List<MahasiswaModel>>(){}.getType());
+
+                RecyclerView.LayoutManager lm = new LinearLayoutManager(MainActivity.this);
+                _recyclerView1.setLayoutManager(lm);
+
+                MahasiswaAdapter ma = new MahasiswaAdapter(mahasiswaModelList);
+                _recyclerView1.setAdapter(ma);
+
+                String mahasiswaCount = "Total Mahasiswa : " + ma.getItemCount();
+                _txtMahasiswaCount.setText(mahasiswaCount);
 
             }
 
             @Override
             public void onFailure(int statusCode, Header[] headers, byte[] responseBody, Throwable error) {
-
+                Toast.makeText(getApplicationContext(), error.getMessage(), Toast.LENGTH_LONG).show();
             }
         });
     }
 
-//    private void initAddButton() {
-//        _addButton = findViewById(R.id.addButton);
-//
-//        _addButton.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                Intent intent = new Intent(getApplicationContext(), AddMahasiswaActivity.class);
-//                startActivity(intent);
-//
-//                loadRecyclerView();
-//            }
-//        });
-//    }
+    private void initAddButton() {
+        _addButton = findViewById(R.id.addButton);
+
+        _addButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), AddMahasiswaActivity.class);
+                startActivity(intent);
+
+                loadRecyclerView();
+            }
+        });
+    }
 }
